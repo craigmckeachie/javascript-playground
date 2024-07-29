a8eb599546b26cc2dfe143201560ad537ca8048b
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 14:23:16 -0400

    for loop

---
+++ loops/for.js
@@ -0,0 +1,18 @@
+let students = [
+  "Emily",
+  "James",
+  "Michael",
+  "Angelica",
+  "Demetri",
+  "LC",
+  "Karol",
+  "Sheryl",
+  "Eric",
+  "Deckard",
+  "Shane",
+];
+
+for (let index = 0; index < students.length; index++) {
+  const student = students[index];
+  console.log(student);
+}
