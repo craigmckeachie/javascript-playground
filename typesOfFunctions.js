9d01256fec9105755da959fcfce10a1694a43c00
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 11:48:40 -0400

    types of functions

---
+++ typesOfFunctions.js
@@ -0,0 +1,13 @@
+//function declaration
+function multiply1(x, y) {
+  return x * y;
+}
+
+console.log(multiply1(10, 5));
+
+//function expression assigning an anonymous function into a variable
+let multiply2 = function (x, y) {
+  return x * y;
+};
+
+console.log(multiply2(5, 5));
