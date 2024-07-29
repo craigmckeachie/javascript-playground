781575394a33d7631a0339cd58196cc128919f35
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 12:00:04 -0400

    more function types

--- typesOfFunctions.js
+++ typesOfFunctions.js
@@ -16,3 +16,26 @@ console.log(multiply2(5, 5));
 let multiply3 = (x, y) => x * y;
 
 console.log(multiply3(5, 3));
+
+let multiply4 = (x, y) => {
+  console.log(x, y);
+  return x * y;
+};
+
+console.log(multiply4(5, 3));
+
+function greeter1(firstName) {
+  console.log("Hi " + firstName);
+}
+
+greeter1("Emily");
+
+
+function greeter2(firstName) {
+  console.log("Hi " + firstName);
+}
+
+greeter1("James");
+
+
+
