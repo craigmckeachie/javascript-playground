adef31217e62c60bd4b9b7d418d1f0b2aadcec2c
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 14:34:52 -0400

    loops 2

--- loops/for.js
+++ loops/for.js
@@ -1,18 +1,25 @@
-let students = [
-  "Emily",
-  "James",
-  "Michael",
-  "Angelica",
-  "Demetri",
-  "LC",
-  "Karol",
-  "Sheryl",
-  "Eric",
-  "Deckard",
-  "Shane",
-];
+function doIt() {
+  var index;
+  let students = [
+    "Emily",
+    "James",
+    "Michael",
+    "Angelica",
+    "Demetri",
+    "LC",
+    "Karol",
+    "Sheryl",
+    "Eric",
+    "Deckard",
+    "Shane",
+  ];
+      //init ;    //condition ;             //increment;
+  for (index = 0; index < students.length; index++) {
+    const student = students[index];
+    console.log(student);
+  }
 
-for (let index = 0; index < students.length; index++) {
-  const student = students[index];
-  console.log(student);
+  console.log(index);
 }
+
+doIt();
---
+++ loops/while.js
@@ -0,0 +1,23 @@
+let students = [
+  "Emily",
+  "James",
+  "Michael",
+  "Angelica",
+  "Demetri",
+  "LC",
+  "Karol",
+  "Sheryl",
+  "Eric",
+  "Deckard",
+  "Shane",
+];
+
+let index = 0; //init
+
+        //condition
+while (index < students.length) {
+  let student = students[index];
+  console.log(student);
+
+  index++; //increment
+}
