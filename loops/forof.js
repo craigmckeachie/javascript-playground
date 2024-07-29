254e5c5f85d4166e4ff453dbc2b319e24e8dade7
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 15:57:25 -0400

    nba teams loop example

---
+++ loops/forof.js
@@ -0,0 +1,23 @@
+let students = [
+  "Emily",
+  "James",
+  "Michael",
+  "Angelica",
+  "Demetri",
+  "LC",
+  "Karol",
+  "Sheryl",
+  "Eric",
+  "Deckard",
+  "Shane",
+];
+
+// //init ;    //condition ;             //increment;
+// for (index = 0; index < students.length; index++) {
+//   const student = students[index];
+//   console.log(student);
+// }
+
+for (const student of students) {
+  console.log(student);
+}
