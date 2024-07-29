0c784632020a30b68fd8006ca674ad8c62e83afc
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 11:09:44 -0400

    mailing label

--- more_functions.js
+++ more_functions.js
@@ -1,7 +1,19 @@
 function displayMailingLabel(name, address, city, state, zip) {
   console.log(name);
   console.log(address);
-  console.log(city + ", " +state + " " + zip);
+  console.log(city + ", " + state + " " + zip);
 }
 
-displayMailingLabel("Craig McKeachie", "232 Soneta Street", "New Albany", "OH", 43065);
+// displayMailingLabel("Craig McKeachie", "232 Soneta Street", "New Albany", "OH", 43065);
+
+function formatMailingLabel(name, address, city, state, zip) {
+  return `
+        ${name}
+        ${address}
+        ${city}, ${state} ${zip}
+  `;
+}
+
+
+let mailingLabel = formatMailingLabel("Craig McKeachie", "232 Soneta Street", "New Albany", "OH", 43065);
+console.log(mailingLabel);
