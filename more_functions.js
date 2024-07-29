390dcd1866f7efaf6838aaa1e9d27c723d0d60d0
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 10:58:15 -0400

    display mailing label

---
+++ more_functions.js
@@ -0,0 +1,7 @@
+function displayMailingLabel(name, address, city, state, zip) {
+  console.log(name);
+  console.log(address);
+  console.log(city + ", " +state + " " + zip);
+}
+
+displayMailingLabel("Craig McKeachie", "232 Soneta Street", "New Albany", "OH", 43065);
