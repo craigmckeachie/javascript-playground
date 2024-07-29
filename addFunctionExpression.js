278ace9711d5a82e6b09384334ec779e9545d5ba
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 11:40:02 -0400

    function stuff

--- addFunctionExpression.js
+++ addFunctionExpression.js
@@ -1,10 +1,8 @@
-//function declaration
-function add(number1, number2) {
+//function expression
+let add = function (number1, number2) {
   let result = number1 + number2;
   return result;
-}
+};
 
 let sum = add(100, 200);
 console.log(sum);
-
-
