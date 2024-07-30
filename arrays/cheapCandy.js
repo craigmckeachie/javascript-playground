36ab5fdd33cd8aea2eb49e41c75329c12409f7ff
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 21:27:16 -0400

    cheap candies starter

--- arrays/cheapCandy.js
+++ arrays/cheapCandy.js
@@ -6,19 +6,7 @@ let products = [
 ];
 
 // Which candies cost less than $4.00?
-const cheapCandies = products.filter((p) => p.price < 4);
-console.log(cheapCandies);
 
 // Which candies have "M&M" in its name?
-const mmCandies = products.filter((p) => p.name.includes("M&Ms"));
-console.log(mmCandies);
 
 // Do we carry "Swedish Fish"?
-const swedishFish = products.find((p) => p.product == "Swedish Fish");
-
-let answer = "Ask again later.";
-if (swedishFish) {
-  console.log("Yes, we carry the delicious fishes.");
-} else {
-  console.log("No, we do not have Swedish Fish.");
-}
