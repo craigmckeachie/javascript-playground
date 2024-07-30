100cf4ea6150507c8e5d1c63032814e6bc65e8a1
Craig McKeachie <craigdmckeachie@gmail.com>
2024-07-29 21:31:54 -0400

    exercise explanation

--- arrays/cheapCandy.js
+++ arrays/cheapCandy.js
@@ -1,3 +1,9 @@
+//Cheap Candy Exercise
+//Create a copy of this file and use array methods (functions attached to the array)
+//to answer each question using code.
+//Your code and not your brain should answer the question.
+//If your code is correct you should be able to change the data but NOT your code and still get the correct answer.
+
 let products = [
   { name: "Gummy Worms", price: 5.79 },
   { name: "Plain M&Ms", price: 2.89 },
